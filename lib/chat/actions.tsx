43b025b1114054d8138d2f4aae7803c4130cfa46
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-07-12 13:52:05 -0500

    Update actions.tsx

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -127,9 +127,9 @@ async function submitUserMessage(content: string) {
   let textNode: undefined | React.ReactNode
 
   const result = await streamUI({
-    model: openai('GPT-4o),
+    model: openai('gpt-3.5-turbo'),
     initial: <SpinnerMessage />,
-system: `\
+    system: `\
     You are a stock trading conversation bot and you can help users buy stocks, step by step.
     You and the user can discuss stock prices and the user can adjust the amount of stocks they want to buy, or place an order, in the UI.
     
