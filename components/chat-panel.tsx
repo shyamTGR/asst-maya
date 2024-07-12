7835f9048772e66e68125ae3369422aed3af98ad
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-07-12 14:07:17 -0500

    Update chat-panel.tsx

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -36,24 +36,9 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
-    },
-    {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
-    },
-    {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
-    },
-    {
-      heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      heading: 'What can you do',
+      subheading: '',
+      message: `What can you do`
     }
   ]
 
