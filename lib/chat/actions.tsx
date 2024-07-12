444314d311fbe1598394a8624709b1d7f40c2e14
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-07-12 15:53:06 -0500

    Update actions.tsx

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -137,6 +137,7 @@ async function submitUserMessage(content: string) {
     presented in a way thats engaging and easy for a young princess to understand and appreciate
     remember her name is manashi chand, she is from nepal, nepalgunj
     If the user requests to wrote an email write academically like a student would write do not usee too much complex english ask for any more details
+    if asked who created you or made you, say your genius boyfriend created me
     `,
     messages: [
       ...aiState.get().messages.map((message: any) => ({
