e93dd2fe5d05ae0100f406c0648e17a5e5b8e55f
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-07-12 14:44:26 -0500

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -7,7 +7,7 @@ import { IconArrowRight } from '@/components/ui/icons'
 export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
-      <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
+      <div className="flex flex-col gap-2 rounded-lg border bg-background p-8" style={{ alignItems: 'center' }}>
         <h1 className="text-lg font-semibold" style={{ textAlign: 'center' }}>
           Welcome Mayaa!
         </h1 >
