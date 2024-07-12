aedb98a21d46310909f5751d3678b6b5e50a8344
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-07-12 14:39:26 -0500

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -8,10 +8,10 @@ export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
-        <h1 className="text-lg font-semibold">
+        <h1 className="text-lg font-semibold" style={{ textAlign: 'center' }}>
           Welcome Mayaa!
-        </h1>
-<img src="https://static.vecteezy.com/system/resources/previews/025/279/123/original/sunflower-bouquet-illustration-decor-png.png" alt="Description of Image" className="responsive-img" />
+        </h1 >
+<img src="https://static.vecteezy.com/system/resources/previews/025/279/123/original/sunflower-bouquet-illustration-decor-png.png" alt="Description of Image" className="responsive-img" style={{ display: 'block', maxWidth: '300px', width: '100%', height: 'auto' }} />
       </div>
       <style jsx>{`
         .responsive-img {
