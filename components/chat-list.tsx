31b258883f51d8a2321ee912b5af91582bd51c2f
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-07-12 15:43:44 -0500

    Update chat-list.tsx

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -19,13 +19,6 @@ export function ChatList({ messages, session, isShared }: ChatList) {
     <div className="relative mx-auto max-w-2xl px-4">
       {!isShared && !session ? (
         <>
-          <div className="group relative mb-4 flex items-start md:-ml-12">
-            <div className="bg-background flex size-[25px] shrink-0 select-none items-center justify-center rounded-md border shadow-sm">
-            </div>
-            <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
-            </div>
-          </div>
-          <Separator className="my-4" />
         </>
       ) : null}
 
