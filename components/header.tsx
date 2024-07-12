a8bcf8d79c0747da627a45def021ed5a0264e2a3
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-07-12 15:41:49 -0500

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -53,8 +53,7 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
-<img src="@/components/logo.png" alt="" className="responsive-img" />
-        Assistant for Mayaa
+        Mayaa's Assistant
       </div>
       
     </header>
